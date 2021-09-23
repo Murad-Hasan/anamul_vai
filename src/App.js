007bb934dd918ff630bc99f43cf9ca1457b5ec22
Murad-Hasan <muradhasan.7845@gmail.com>
2021-09-23 11:31:56 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,116 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Carousel
+        additionalTransfrom={0}
+        arrows
+        autoPlay
+        autoPlaySpeed={3000}
+        centerMode={false}
+        className=""
+        containerClass="container"
+        dotListClass=""
+        draggable
+        focusOnSelect={false}
+        infinite
+        itemClass=""
+        keyBoardControl
+        minimumTouchDrag={80}
+        renderButtonGroupOutside={false}
+        renderDotsOutside={false}
+        responsive={{
+          desktop: {
+            breakpoint: {
+              max: 3000,
+              min: 1024,
+            },
+            items: 1,
+          },
+          mobile: {
+            breakpoint: {
+              max: 464,
+              min: 0,
+            },
+            items: 1,
+          },
+          tablet: {
+            breakpoint: {
+              max: 1024,
+              min: 464,
+            },
+            items: 1,
+          },
+        }}
+        showDots
+        sliderClass=""
+        slidesToSlide={1}
+        swipeable
+      >
+        <div className="slider slide_one_bg">
+          <div>
+            <h2>Lorem ipsum dolor sit amet consectetur.</h2>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Voluptas, nemo?
+            </p>
+            <button>Find More</button>
+            <form action="">
+              <input type="text" />
+            </form>
+          </div>
+        </div>
+
+        <div className="slider slide_two_bg">
+          <div>
+            <h2>Lorem ipsum dolor sit amet consectetur.</h2>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Voluptas, nemo?
+            </p>
+            <button>Find More</button>
+            <form action="">
+              <input type="text" />
+            </form>
+          </div>
+        </div>
+        <div className="slider slide_three_bg">
+          <div>
+            <h2>Lorem ipsum dolor sit amet consectetur.</h2>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Voluptas, nemo?
+            </p>
+            <button>Find More</button>
+            <form action="">
+              <input type="text" />
+            </form>
+          </div>
+        </div>
+
+        <div className="slider slide_four_bg">
+          <div>
+            <h2>Lorem ipsum dolor sit amet consectetur.</h2>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Voluptas, nemo?
+            </p>
+            <button>Find More</button>
+            <form action="">
+              <input type="text" />
+            </form>
+          </div>
+        </div>
+
+        {/* <img
+          src="https://images.unsplash.com/photo-1550338861-b7cfeaf8ffd8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+          alt="Fourth slide"
+        /> */}
+      </Carousel>
+    </>
   );
 }
 
